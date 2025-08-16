252d17779fa20f5cdda1eae974159728b9380442
Aghav Rajesh <aghavrajesh8@gmail.com>
2025-08-16 12:25:15 +0530

    la

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyparser = require("body-parser");
 var upload = require("express-fileupload");
 var session = require("express-session");
-require("dotenv")
+require("dotenv");
 
 var user_route = require("./routes/user_route");
 var admin_route = require("./routes/admin_route")
