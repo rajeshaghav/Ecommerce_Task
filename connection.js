8459e7af959821c11fa60111097a5fd7e43ee72f
Rajesh Aghav <aghavrajesh8@gmail.com>
2025-08-16 11:52:59 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 var util = require("util")
 
 var conn = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"ecommerce_task"
+    host:"bs451v8prn0isgpwvd7r-mysql.services.clever-cloud.com",
+    user:"ud5wch3g7kvzassf",
+    password:"fNqOcfXi1ORQiAglNNWE",
+    database:"bs451v8prn0isgpwvd7r"
 })
 
 var exe =  util.promisify(conn.query).bind(conn)
